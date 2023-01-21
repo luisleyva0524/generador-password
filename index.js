b93dbbf599d5372e78342054fff9dacb3499d90e
luisleyva0524 <luisantonioleyva0524@gmail.com>
2023-01-20 17:31:33 -0700

    main

--- index.js
+++ index.js
@@ -1,78 +1,44 @@
-const isDarkMode = document.querySelector('#switch')
 const $sizePassword = document.getElementById('field-password')
 const $length = document.querySelector('#length')
 const $generatePassword = document.getElementById('new-password')
 const $outputPassword = document.getElementById('output-password')
 const $colorSize = document.getElementById('bar-progress')
-// const $savePassword = document.getElementById('save-password')
-// const $copy = document.getElementById('copy-password')
+const $copyText = document.querySelector('#icon-copy')
 
 
 $sizePassword.addEventListener('change', (event) => {
-    if ($sizePassword.value <= 8) {
-        // $colorSize.innerHTML = 2
+    if ($sizePassword.value == 8) {
         $colorSize.style.backgroundColor = "red"
-        // $colorSize.classList.add('bar-progress')
-        $colorSize.value = 100
+        $colorSize.value = 33.333333
     }
 
     if ($sizePassword.value > 8 && $sizePassword.value <= 16) {
         $colorSize.style.backgroundColor = "yellow"
-        // $colorSize.classList.add('bar-progress-yellow')
-
         $colorSize.value = 66.666666667
-
     }
 
     if ($sizePassword.value > 16) {
-        // $colorSize.classList.add('bar-progress-green')
-        // $colorSize.value = 100
         $colorSize.style.backgroundColor = "green"
         $colorSize.innerHTML = 100
     }
 })
 
-
-
-const displaySize = () => {
+$length.innerHTML = 8
+$sizePassword.addEventListener('change', () => {
     $length.innerHTML = $sizePassword.value
+})
 
 
+$generatePassword.addEventListener('click', () => {
+    $outputPassword.value = generarContrasena(Number($sizePassword.value))
+})
 
-
-
-
-    $sizePassword.addEventListener('change', (e) => {
-        $length.innerHTML = e.target.value
-    })
-}
-
-
-const updateActions = () => {
-    $generatePassword.addEventListener('click', () => {
-        $outputPassword.value = generarContrasena(Number($sizePassword.value))
-    })
-}
-
-displaySize()
-updateActions()
-
-
-
-
-
-
-// const darkmode = () => {
-//     const elementBody = document.body
-//     elementBody.classList.toggle('.switch')
-// }
-
-// if (isDarkMode.checked) {
-//     darkmode()
-//     console.log('click');
-//     // darkmode()
-// }
-
+$copyText.addEventListener('click', (e) => {
+    e.preventDefault()
+    $outputPassword.select()
+    navigator.clipboard.writeText($outputPassword.value)
+    alert("texto copiado")
+})
 
 
 const generarContrasena = (numCaracteres) => {
